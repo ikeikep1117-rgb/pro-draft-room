05edba1c25178182e9e6814536f4b651736f14bd
ikeikep1117-rgb <ikeikep1117@gmail.com>
2026-06-22 01:31:46 +0900

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Firebase Console > プロジェクトの設定 > マイアプリ > SDK の設定と構成
+// に表示される値へ置き換えてください。
+export const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.firebasestorage.app",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID",
+};
